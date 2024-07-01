761b76911858c9a475dff3272a646b76e939f9e2
konradlesniak <kontakt@creativesight.pl>
2024-07-01 13:44:49 +0200

    Module1 Lesson4 - axios interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,12 +2,20 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.headers['request-startTime'] = new Date().getTime();
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
   // Do something with response data
+  const currentTime = new Date().getTime()      
+  const startTime = response.config.headers['request-startTime']    
+
+  response.headers['request-duration'] = currentTime - startTime  
+
+  console.log(response.headers['request-duration']);
+
   return response;
 });
 
