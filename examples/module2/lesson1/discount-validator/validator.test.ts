fbfcc6c88e6d7abe5cf811f06fbbec3824f5d5c8
konradlesniak <kontakt@creativesight.pl>
2024-07-06 18:45:21 +0200

    Module2 Lesson1

--- examples/module2/lesson1/discount-validator/validator.test.ts
+++ examples/module2/lesson1/discount-validator/validator.test.ts
@@ -2,18 +2,29 @@ import { describe, expect, test } from 'vitest';
 import { formValidator } from './validator';
 
 describe('Form validation', () => {
+    test('should return an empty error array if all form data filled', () => {
+        const errors = formValidator('John', 'Doe', 30);
+        expect(errors).toEqual([]);
+    });
+
     test('should return an error if first name is missing', () => {
         const errors = formValidator('', 'Doe', 30);
         expect(errors).toContain('First name is required');
+        expect(errors).toContain('First name require at least 1 character');
     });
 
     test('should return an error if last name is missing', () => {
         const errors = formValidator('John', '', 30);
         expect(errors).toContain('Last name is required');
+        expect(errors).toContain('Last name require at least 1 character');
     });
 
     test('should return an error if age is negative', () => {
         const errors = formValidator('John', 'Doe', -1);
         expect(errors).toContain('Age must be a positive number');
+    });
+
+    test('should return an error if age is not a number', () => {
+        expect(() => formValidator('John', 'Doe', '1')).toThrow('Age must be a number');
     });
 });
--- examples/module2/lesson1/discount-validator/validator.ts
+++ examples/module2/lesson1/discount-validator/validator.ts
@@ -9,12 +9,24 @@ export function formValidator(
     errors.push('First name is required');
   }
 
+  if (firstName?.length === 0) {
+    errors.push('First name require at least 1 character');
+  }
+
   if (!lastName) {
     errors.push('Last name is required');
   }
 
+  if (lastName?.length < 1) {
+    errors.push('Last name require at least 1 character');
+  }
+
   if (age < 0) {
     errors.push('Age must be a positive number');
+  }
+
+  if (typeof age !== 'number') {
+    throw new Error('Age must be a number');
   }
 
   return errors;
