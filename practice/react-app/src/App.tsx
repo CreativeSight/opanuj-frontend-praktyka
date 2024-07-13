0250e5c646d941970b5388fb817b0473674fb256
konradlesniak <kontakt@creativesight.pl>
2024-07-13 19:36:04 +0200

    Module 2 Lesson 3 open API

--- practice/react-app/src/App.tsx
+++ practice/react-app/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { DefaultApi } from '../api-client/apis/DefaultApi';
+import './App.css';
+import { Character } from '../api-client/models';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<Character[]>([]);
+  const api = new DefaultApi();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    api
+      .getCharacters()
+      .then((res) => setData(res.results!))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return data.map((item) => <div key={item.id}>{item.name}</div>);
 }
 
-export default App
+export default App;
